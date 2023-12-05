bdb1c92aff399e21a0c34666abe7470d64f2aeeb
Jmikyung <rladudwp421@naver.com>
2023-12-05 21:17:42 +0900

    feat. password_error_check

--- src/main/resources/static/js/join.js
+++ src/main/resources/static/js/join.js
@@ -1,3 +1,19 @@
 function joinHospital(){
-	document.getElementById("joinForm").submit();
+	
+	if(validatePassword()){
+		document.getElementById("joinForm").submit();
+	}
+
+}
+
+function validatePassword() {
+  var passwd = document.getElementById("user_pw").value;
+  var repeatPasswd = document.getElementById("user_pw_re").value;
+
+  if (passwd != repeatPasswd) {
+    alert("패스워드가 일치하지 않습니다.");
+    return false;
+  }
+
+  return true;
 }
